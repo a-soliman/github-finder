ce1c238c79bb05399089fc3ce4f181cd23549520
a-soliman <ahmed.soliman@programmer.net>
2018-06-21 20:50:27 -0700

    coded getRepos  method

--- js/github.js
+++ js/github.js
@@ -2,6 +2,8 @@ class Github {
     constructor() {
         this.client_id = '5224216c6364edc139fc';
         this.client_secret = 'aa1ae5eda22cea92a259c4ab8393439f1f226bcb';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     // GET USER
@@ -14,6 +16,9 @@ class Github {
 
     // GET REPOS
     getRepos( user ) {
-        
+        return fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+            .then(res => res.json())
+            .then(data => data)
+            .catch(err => err);
     }
 }
